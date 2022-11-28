17dcb807857d1e4fc496da28c9d6ed4d11a9aa69
iramos <iramos@106-24.info2>
2022-11-28 09:18:22 +0100

    no lo se

---
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio05.java
@@ -0,0 +1,52 @@
+package ejercicios;
+
+public class Ejercicio05 {
+	
+	/* Diseña tres funciones de nombre numerosAleatorios que hagan lo siguiente:
+
+
+	/* a) La primera de las funciones recibirá un argumento de tipo entero que indicará la 
+	 * cantidad de números aleatorios a generar. Esta función generará números aleatorios comprendidos 
+	 * entre 0 y 1 y los mostrará por pantalla.
+	 */
+	public static int numerosAleatorios (int numAleatorios) {
+		
+		for (i= )
+		Math.random();
+		return aleatorios;
+		
+	}
+	
+	
+	/* b) La segunda de las funciones recibirá dos argumentos de tipo entero: 
+	- El primero indica la cantidad de números aleatorios a generar
+	- El segundo indica el valor máximo que tomará el número aleatorio. 
+	Los números aleatorios generados deben estar entre 0 y el valor máximo introducido por parámetro. 
+	En este caso los números aleatorios deben ser enteros. Los números aleatorios generados deben mostrarse por pantalla.
+	*/
+	
+	public static int numerosAleatorios (int cantAleatorios, int vMax) {
+		vMax = 1;
+		return vMax;
+		// falta cantAleatorios
+		
+	}
+	
+	
+	/*c) La tercera de las funciones recibirá tres argumentos de tipo entero: 
+		- El primero indica la cantidad de números aleatorios a generar
+		- El segundo indica el valor mínimo que tomará el número aleatorio. 
+		- El tercero indica el valor máximo que tomará el número aleatorio.
+		Los números aleatorios generados deben estar entre el valor mínimo y el valor máximo introducidos por parámetro. En este caso los números aleatorios deben ser enteros. Los números aleatorios generados deben mostrarse por pantalla.
+		*/
+		
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+		
+	}
+
+}
