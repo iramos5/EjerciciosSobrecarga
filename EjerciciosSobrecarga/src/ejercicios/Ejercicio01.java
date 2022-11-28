ba61bacd827484f6680305d53fbc171ac1e20588
iramos <iramos@106-24.info2>
2022-11-28 08:53:26 +0100

    MIRAR BIEN

--- EjerciciosSobrecarga/src/ejercicios/Ejercicio01.java
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio01.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
 	/* Diseña dos funciones de nombre suma que hagan lo siguiente:
 	a) La primera de las funciones recibirá dos argumentos de tipo entero y devolverá la suma de ambos. El valor a devolver también será entero.
@@ -16,6 +18,21 @@ public class Ejercicio01 {
 	
 	
 	public static void main(String[] args) {
+		int numInt1, numInt2;
+		double numDouble1, numDouble2;
+		int resInt;
+		double resDouble;
+		
+		Scanner scan = new Scanner (System.in);
+		numInt1 = scan.nextInt();
+		numInt2= scan.nextInt();
+		numDouble1 = scan.nextDouble();
+		numDouble2 = scan.nextDouble();
+		
+		resInt = suma (numInt1 + numInt2);
+		resDouble = suma (numDouble1 + numDouble2);
+		
+		scan.close();
 		
 
 	}
---
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio03.java
@@ -0,0 +1,25 @@
+package ejercicios;
+
+public class Ejercicio03 {
+	
+	/* Diseña dos funciones de nombre sumaEnteros que hagan lo siguiente:
+	a) La primera de las funciones recibirá un argumento (numero) de tipo entero y devolverá la suma de todos los números desde 1 hasta numero. El valor a devolver debe ser entero.
+	b) La segunda de las funciones recibirá dos argumentos (num1 y num2) de tipo entero y devolverá la suma de todos los números comprendidos entre los dos números (num1 y num2). El tipo a devolver debe ser entero.
+	*/
+	
+	public static int sumaEnteros (num1, suma) {
+		
+		for (i = num1; i>=1; i-- ) {
+			suma 
+		}
+			
+	}
+	
+
+	public static void main(String[] args) {
+		
+	}
+	
+	
+
+}
