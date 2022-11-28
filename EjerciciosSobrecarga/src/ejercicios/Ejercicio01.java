58be7608145bea08ba5b168b8b9a88d7609bdb98
iramos <iramos@106-24.info2>
2022-11-28 08:36:02 +0100

    update

--- EjerciciosSobrecarga/src/ejercicios/Ejercicio01.java
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio01.java
@@ -1,6 +1,6 @@
 package ejercicios;
 
-public class Ejercicio1 {
+public class Ejercicio01 {
 	/* Diseña dos funciones de nombre suma que hagan lo siguiente:
 	a) La primera de las funciones recibirá dos argumentos de tipo entero y devolverá la suma de ambos. El valor a devolver también será entero.
 	b) La segunda de las funciones recibirá dos argumentos de tipo double y devolverá la suma de ambos. El tipo a devolver debe ser también double.
