613978cd816201276041fce6a39b23f521164b21
iramos <iramos@106-24.info2>
2022-11-28 08:34:29 +0100

    sobrecargas

---
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio02.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class Ejercicio02 {
+
+	/* Diseña dos funciones de nombre media que hagan lo siguiente:
+	a) La primera de las funciones recibirá dos argumentos de tipo entero y devolverá la media de ambos. El valor a devolver debe ser double.
+	b) La segunda de las funciones recibirá tres argumentos de tipo entero y devolverá la media de los tres. El tipo a devolver debe ser double.
+	*/
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EjerciciosSobrecarga/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+public class Ejercicio1 {
+	/* Diseña dos funciones de nombre suma que hagan lo siguiente:
+	a) La primera de las funciones recibirá dos argumentos de tipo entero y devolverá la suma de ambos. El valor a devolver también será entero.
+	b) La segunda de las funciones recibirá dos argumentos de tipo double y devolverá la suma de ambos. El tipo a devolver debe ser también double.
+	*/
+
+	public static int suma (int a, int b) {
+		return a+b;
+	}
+	
+	public static double suma (double a, double b) {
+		return a+b;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+
+}
+
+
